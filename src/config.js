d919a3ea3061eae7e87eee9a2c6f66b162e526b8
Tiago Loureiro <tiago.loureiro@dipcode.com>
2024-01-17 16:03:38 +0000

    added new rules

--- src/config.js
+++ src/config.js
@@ -7,5 +7,22 @@ module.exports = {
   rules: {
     'no-descending-specificity': null,
     'order/properties-alphabetical-order': true,
+    'scale-unlimited/declaration-strict-value': [['color', 'background-color', 'font-weight', 'font-size']],
+    'declaration-no-important': true,
+    'unit-allowed-list': ['rem', { ignoreFunctions: ['rem'], severity: 'warning' }],
+    // TODO: This should move to error in the future.
+    'max-nesting-depth': [3, { severity: 'warning' }],
+    // TODO: This should move to error in the future.
+    'selector-max-attribute': [1, { severity: 'warning' }],
+    // TODO: This should move to error in the future.
+    'selector-max-combinators': [3, { severity: 'warning' }],
+    // TODO: This should move to error in the future.
+    'selector-max-id': [1, { severity: 'warning' }],
+    // TODO: This should move to error in the future.
+    'selector-max-pseudo-class': [2, { severity: 'warning' }],
+    // TODO: This should move to error in the future.
+    'selector-max-type': [1, { severity: 'warning' }],
+    // TODO: This should move to error in the future.
+    'selector-max-universal': [1, { severity: 'warning' }],
   },
 };
