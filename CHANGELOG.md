5cb9507ab583e404181f274eaa4318f227210120
Tiago Loureiro <tiagoloureiro@users.noreply.github.com>
2024-01-25 16:13:31 +0000

    Fix/GitHub workflow (#4)
    * added new rules
    
    * fix github workflows
    
    * fix to warning about 'declaration-strict-value' not having autoFix function
    
    * fix to warning about 'declaration-strict-value' not having autoFix function
    
    * sync package and lock
    
    * added rules for vendor prefixes
    
    * added rules for vendor prefixes
    
    * Refined exclude for 'declaration-strict-value' and 'unit-allowed-list'
    
    * Refined exclude for 'declaration-strict-value' and 'unit-allowed-list'

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,17 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [v0.1.2] - 2024-01-24
+
+### Added
+
+- Rules for vendor prefixes.
+- Refined excludes for 'declaration-strict-value' and 'unit-allowed-list'
+
+### Fixed
+
+- Warning about 'declaration-strict-value' not having autoFix function.
+
 ## [v0.1.1] - 2024-01-17
 
 ### Added
@@ -19,5 +30,6 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 - First release
 
+[v0.1.2]: https://www.npmjs.com/package/@dipcode/stylelint-config/v/0.1.2
 [v0.1.1]: https://www.npmjs.com/package/@dipcode/stylelint-config/v/0.1.1
 [v0.1.0]: https://www.npmjs.com/package/@dipcode/stylelint-config/v/0.1.0
--- src/config.js
+++ src/config.js
@@ -1,33 +1,56 @@
 'use strict';
 
 // See https://stylelint.io/user-guide/rules/.
+/** @type {import('stylelint').Config} */
 module.exports = {
   extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier-scss'],
   plugins: ['stylelint-order', 'stylelint-declaration-strict-value'],
   rules: {
+    'property-no-vendor-prefix': true,
+    'value-no-vendor-prefix': true,
+    'selector-no-vendor-prefix': true,
+    'media-feature-name-no-vendor-prefix': true,
+    'at-rule-no-vendor-prefix': true,
     'no-descending-specificity': null,
     'order/properties-alphabetical-order': true,
     'scale-unlimited/declaration-strict-value': [
-      ['color', 'background-color', 'font-weight', 'font-size', '/margin/', '/padding/'],
+      [
+        'color',
+        'background-color',
+        'font-weight',
+        'font-size',
+        '/margin/',
+        '/padding/',
+        'top',
+        'bottom',
+        'left',
+        'right',
+      ],
       {
         ignoreValues: {
-          'color': ['transparent', 'unset', 'inherit'],
-          'background-color': ['transparent', 'unset', 'inherit'],
-          '/margin/': ['unset', 0, 'inherit', 'auto'],
-          '/padding/': ['unset', 0, 'inherit'],
+          'color': ['transparent', 'unset', 'inherit', 'initial'],
+          'background-color': ['transparent', 'unset', 'inherit', 'initial'],
+          '/margin/': ['unset', 0, 'inherit', 'auto', 'initial'],
+          '/padding/': ['unset', 0, 'inherit', 'initial', '/[0-9]+%/'],
+          'top': ['unset', 0, 'inherit', 'auto', 'initial', '/[0-9]+%/'],
+          'bottom': ['unset', 0, 'inherit', 'auto', 'initial', '/[0-9]+%/'],
+          'left': ['unset', 0, 'inherit', 'auto', 'initial', '/[0-9]+%/'],
+          'right': ['unset', 0, 'inherit', 'auto', 'initial', '/[0-9]+%/'],
         },
+        disableFix: true,
       },
     ],
     'declaration-no-important': true,
     'unit-allowed-list': [
-      ['rem', 's', 'ms'],
+      ['rem', 's', 'ms', '%'],
       {
         ignoreFunctions: ['/.*/'],
         severity: 'warning',
         ignoreProperties: {
-          '%': ['min-width', 'max-width', 'width', 'min-height', 'max-height', 'height', 'flex-basis', 'flex'],
-          'vw': ['min-width', 'max-width', 'width', 'min-height', 'max-height', 'height', 'flex-basis', 'flex'],
-          'vh': ['min-width', 'max-width', 'width', 'min-height', 'max-height', 'height', 'flex-basis', 'flex'],
+          vw: ['/width/', '/height/', '/flex/'],
+          vh: ['/width/', '/height/', '/flex/'],
+          fr: ['/grid/'],
+          deg: ['rotate'],
         },
       },
     ],
